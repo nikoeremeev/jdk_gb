389fe73bcbde9b82ff8ac447b40d7e8607d125c8
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-09-28 20:48:52 +0300

    draw Line

--- src/main/java/ru/example/lessons/lesson_01/Map.java
+++ src/main/java/ru/example/lessons/lesson_01/Map.java
@@ -5,11 +5,24 @@ import java.awt.*;
 
 public class Map extends JPanel{
     Map() {
-        setBackground(Color.BLACK);
+        setBackground(Color.WHITE);
     }
 
     void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
-        System.out.printf("Mode: %d;\nSize: x=%d, y=%d;\nWin Length: %d",
+        System.out.printf("Mode: %d;\nSize: x=%d, y=%d;\nWin Length: %d\n",
                 mode, fSzX, fSzY, wLen);
     }
+
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        render(g);
+    }
+
+    private void render(Graphics g){
+        g.setColor(Color.BLACK);
+        g.drawLine(0, 0, 100, 100);
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+    }
 }
+
